c7b8483773624cf1a1485d49cbfd84091b98a4fc
bernatsampera <b.sampera@gmail.com>
2020-07-14 20:46:10 +0200

    Add styles palette and adjust main App styles

---
+++ src/modules/Styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+import { color } from '../shared/styles';
+
+export const StyledForm = styled.form`
+  padding: 4% 0;
+`;
+
+export const TranslationContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+export const StyledDrawer = styled.div`
+  top: auto;
+  padding: 35px;
+  background: ${color.blackShadows};
+  border: 2px solid ${color.blackShadows};
+`;
+
+
