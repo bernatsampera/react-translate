8323be995bd1e6aa726210168d1e493964fc8af6
bernatsampera <b.sampera@gmail.com>
2020-07-15 20:35:39 +0200

    remove css import

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Link  } from 'react-router-dom';
-import './App.css';
 
 import modules from './modules';
 import NavBar from './components/NavBar';
