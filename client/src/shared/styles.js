947932a5646a81df4c12b6c4e3ff593e752fdb8d
bernatsampera <b.sampera@gmail.com>
2020-07-15 20:32:24 +0200

    update webapp theme

--- client/src/shared/styles.js
+++
@@ -1,8 +0,0 @@
-export const color = {
-  blackShadows: '#c2b2b4ff',
-  englishViolet: '#6b4e71ff',
-  charcoal: '#3a4454ff',
-  darkElectricBlue: '#53687eff',
-  mistyRose: '#f5ddddff',
-};
-
---
+++ client/src/shared/theme.js
@@ -0,0 +1,17 @@
+export const color = {
+  Text: '#eceff1',
+  Primary: '#233545',
+  Secondary: '#4d5f70',
+  Dark: '#000e1e',
+  Light: '#F3F3F4',
+  Red: '#ed5565',
+  Orange: '#f8ac59',
+  Blue: '#23c6c8',
+  Green: '#1ab394',
+  DarkBlue: '#1c84c6',
+};
+
+export const size = {
+  history: '15vw',
+}
+
